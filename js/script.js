030b71deb30e1e6a389c435ef2817fd180f53da6
wahidrana2108 <wahidzamanrana@gmail.com>
2022-08-16 22:03:54 +0600

    js starting

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 window.onload = function(){
     document.getElementById('switchToReg').onclick = switchToReg;
     document.getElementById('switchToLogin').onclick = switchToLogin;
+    document.getElementById('login-btn').onclick = loginValidation;
+    document.getElementById('register-btn').onclick = registrationValidation;
 
     function switchToReg(){
         document.getElementById('register-portal').style = "display:inline-block";
@@ -10,5 +12,39 @@ window.onload = function(){
     function switchToLogin(){
         document.getElementById('register-portal').style = "display:none";
         document.getElementById('login-portal').style = "display:inline-block";
+    }
+
+
+    // acc no and pin pattern
+    var accNoPat = "^[0-9]{6}$";
+    var accPinPat = "^[0-9]{4}$";
+
+    // login validation 
+    function loginValidation(){
+        var lAccNo = document.getElementById('lAccNo').value;
+        var lAccPin = document.getElementById('lAccPin').value;
+
+        if (lAccNo.match(accNoPat) && lAccPin.match(accPinPat)) {
+            alert("welcome!");
+        }
+        else{
+            alert("invalid info!");
+        }
+    }
+
+
+     // registration validation 
+     function registrationValidation(){
+        var rAccName = document.getElementById('rAccName').value;
+        var rAccNo = document.getElementById('rAccNo').value;
+        var rAccPin = document.getElementById('rAccPin').value;
+        var rConAccPin = document.getElementById('rConAccPin').value;
+
+        if (rAccNo.match(accNoPat) && rAccPin.match(accPinPat) && rAccName!=null && rAccPin==rConAccPin) {
+            alert("welcome!");
+        }
+        else{
+            alert("Fill all ino correctly!");
+        }
     }
 }
